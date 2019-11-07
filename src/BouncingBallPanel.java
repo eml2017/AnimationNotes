b10b04697fcbf594685848cbe820fd1269ca161b
Larson <elarson5@zagmail.gonzaga.edu>
2019-11-07 14:39:17 -0800

    animation works

--- src/BouncingBallPanel.java
+++ src/BouncingBallPanel.java
@@ -1,20 +1,51 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
 
 public class BouncingBallPanel extends JPanel {
     View view;
     final int DIAMETER = 40;
+    // Upper left-hand corner
+    int x = 0;
+    int y = 0;
+    // dx and dy represent where the ball is going
+    int dx = 2;
+    int dy = 2;
 
-    public BouncingBallPanel(View view) {
-        setBackground(Color.WHITE);
+    // Animation
+    // Change in graphics over time
+    // We need to use a timer
+    // It's listener is ActionListener!
+    static final int DELAY = 1000; // Milliseconds (1000 milliseconds = 1 second)
+    Timer timer;
+
+    public BouncingBallPanel() {
+        // Takes in 1. the time between firings (delay) and 2. a reference to ActionListener
+        //      Called every time Timer fires (every DELAY number of millisecond)
+        timer = new Timer(DELAY, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                // Update the paintComponent
+                repaint();
+            }
+        });
+        // DON'T FORGET TO START THE TIMER
+        timer.start();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
         Graphics2D g2 = (Graphics2D) g;
         g2.setColor(Color.RED);
-        Ellipse2D.Double ball = new Ellipse2D.Double(0, 0, DIAMETER, DIAMETER);
+
+        x += dx;
+        y += dy;
+
+        Ellipse2D.Double ball = new Ellipse2D.Double(x, y, DIAMETER, DIAMETER);
         g2.fill(ball);
     }
 }
--- src/View.java
+++ src/View.java
@@ -4,11 +4,10 @@ import java.awt.*;
 public class View extends JFrame {
     public View() {
         super("Bouncing Ball");
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(500, 500));
         setVisible(true);
-        getContentPane().add(new JPanel());
+        getContentPane().add(new BouncingBallPanel());
         pack();
     }
 }
